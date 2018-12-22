a10775cbf984bf084a87191b765b9f131471a6f9
Mike <michael.p.silverman@gmail.com>
2018-12-21 21:33:59 -0500

    stats strength

--- utils/equipmentGenerationUtils/generateStats.js
+++ utils/equipmentGenerationUtils/generateStats.js
@@ -1,5 +1,26 @@
 getRandomInt = require("./getRandomInt");
 
+// hp [suffix] of the fox, wolf, lion, bear, whale
+// str [suffix] of strength, might, power, giants, titans
+// dex [suffix] of dexterity, proficiancy, finesse, mastery, perfection
+// int [suffix] of intelligence, the mind, brilliance, sorcery, wizardry
+// magicDefense [suffix] of cover, shelter, shielding, dispersal, diffusion
+// armorPiercing [suffix] of piercing, stabbing, penetration, smashing, shattering
+
+// mp [prefix] bluejay's, cockatoo's owl's, kea's, raven's
+// bonusDamage [prefix] jagged, deadly, vicious, brutal, savage
+// bonusArmorClass [prefix] sturdy, strong, robust, reinforced, unyeilding
+// accuracy [prefix] steady, stable, sighted, guided, unwavering
+// magicAccuracy [prefix] cryptic, esoteric, magical, mystical, arcane
+      // [prefix] red, crimson, scarlet, garnet, ruby
+      // [prefix] warm, padded, insulated, heated, frostproof
+      // [prefix] blue, azure, lapis, cobalt, sapphire
+      // [prefix] waterproof, airtight, watertight, sealed, hermetic
+      // [prefix] yellow, amber, saffron, citrine, topaz
+      // [prefix] smooth, sleek, contoured, streamlined, frictionless
+      // [prefix] pure, sublime, hallowed, angelic, holy
+      // [prefix] dim, shadowy, dark, black, void
+
 module.exports = generateStats = (subType, level, rarity) => {
   let stats = {
     hp: 0, // [suffix] of the fox, wolf, lion, bear, whale
@@ -35,15 +56,64 @@ module.exports = generateStats = (subType, level, rarity) => {
   const suffixStats = ["hp", "stats", "magicDefense", "armorPiercing"];
 
   generatePrefixStats = (type, level) => {
+    // Declare the potential prefix and suffix
     switch (type) {
       case "cloth" || "leather" || "mail" || "plate":
+        const potentialPrefixes = ["bonusArmorClass", "mp", "elementalResistance", "magicAccuracy"]
+        const potentialSuffixes = ["str", "dex", "int", "hp", "magic defense"]
       case "1hSword" ||
         "1hClub" ||
         "1hAxe" ||
         "pistol":
+        const potentialPrefixes = ["magicAccuracy", "Accuracy", "bonusDamage", "mp"]
+        const potentialSuffixes = ["str", "dex", "int", "armorPiercing", "hp"]
       case "2hSword" || "2hClub" || "2hAxe" || "polearm" || "bow" || "crossbow" || "rifle":
+        const potentialPrefixes = ["magicAccuracy", "Accuracy", "bonusDamage", "mp", "elementalResistance"]
+        const potentialSuffixes = ["str", "dex", "int", "armorPiercing", "hp", "magic defense"]
       case "shield":
+        const potentialPrefixes= ["bonusArmorClass", "mp", "elementalResistance", "magicAccuracy"]
+        const potentialSuffixes = ["str", "dex", "int", "hp", "magic defense"]
       default:
     }
+    // Roll from list of potential mods
+    const randomPrefix = potentialPrefixes[getRandomInt(potentialPrefixes.length-1)]
+    const randomSuffix = potentialSuffixes[getRandomInt(potentialSuffixes.length-1)]
+    // Determine rolls based on level and eq type
+    switch(randomPrefix){
+      case "hp":
+        stats.hp = level * 2
+      case "str":
+        stats.str = level * 1
+      case "dex":
+        stats.dex = level * 1
+      case "int":
+        stats.int = level * 1
+      case "magicDefense":
+        stats.int = level * 2
+      case "armorPiercing":
+        stats.armorPiercing = level * 1
+      default:
+    }
+    switch(randomSuffix){
+      case "mp":
+        stats.mp = level * 2
+      case "bonusDamage":
+        stats.bonusDamage = level * 1
+      case "bonusArmorClass":
+        stats.bonusArmorClass = level * 1
+      case "accuracy":
+        stats.accuracy = level * 2
+      case "magicAccuracy":
+        stats.magicAccuracy = level * 2
+      case "elementalResistance":
+        const elements = ["fire", "ice", "lightning", "water", "wind", "earth", "light", "dark"]
+        const randomElement = elements[getRandomInt(elements.length-1)]
+        switch(randomElement){
+          case "fire":
+          default:
+        }
+      default:
+    }
+
   };
 };
